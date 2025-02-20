732765487363bcde624bf222fed1245c9cbc4cae
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-02-20 22:40:48 +0900

    helpコマンドのレスポンスををEmbedに変更

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,10 +1,19 @@
-import { SlashCommandBuilder,CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder,CommandInteraction,EmbedBuilder } from 'discord.js';
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Botの説明を表示します'),
 	async execute(interaction:CommandInteraction) {
-		await interaction.reply('Pong!');
+        const helpEmbed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('ロール付与bot')
+            .setDescription('ボタンを押すことでロールを付与することのできるBotです。')
+            .addFields( // 複数個記入可能
+                { name: '`/help`', value: 'ボットの説明を表示します' },
+            )
+		await interaction.reply(
+            {embeds:[helpEmbed]}
+        );
 	},
 };
